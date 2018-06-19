70d199d8283b8d0cc531db1ee4c4cdcf12faaa8c
zhanglianxin <zhanglianxin1993@qq.com>
2018-06-19 09:56:42 +0800

    函数表达式、闭包、方法

--- code/functions.go
+++ code/functions.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "math"
+)
 
 func main() {
     var a int = 1
@@ -9,19 +12,38 @@ func main() {
     c, d := swap(a, b)
     fmt.Println(c, d)
 
-    fmt.Println()
-
     var x, y int = 100, 200
 
-    fmt.Printf("Call by value: ")
+    fmt.Println("Call by value: ")
     swapcallbyvalue(x, y)
     fmt.Printf("x is %d, y is %d", x, y)
 
     fmt.Println()
 
-    fmt.Printf("Call by reference: ")
+    fmt.Println("Call by reference: ")
     swapcallbyreference(&x, &y)
     fmt.Printf("x is %d, y is %d", x, y)
+
+    fmt.Println()
+    fmt.Println("Function as value: ")
+    getSquareRoot := func (x float64) float64 {
+        return math.Sqrt(x)
+    }
+    fmt.Println(getSquareRoot(9))
+
+    fmt.Println("Function closure: ")
+    nextNumber := getSequence()
+    fmt.Println(nextNumber())
+    fmt.Println(nextNumber())
+    fmt.Println(nextNumber())
+
+    nextNumber1 := getSequence()
+    fmt.Println(nextNumber1())
+    fmt.Println(nextNumber1())
+
+    fmt.Println("Methods: (special types of functions)")
+    circle := Circle{x: 0, y: 0, radius: 5}
+    fmt.Printf("Circle area %f", circle.area())
 }
 
 func swap(x int, y string) (string, int) {
@@ -44,4 +66,21 @@ func swapcallbyreference(x *int, y *int) {
     temp = *x
     *x = *y
     *y = temp
+}
+
+// Close over a variable i of upper function to form a closure
+func getSequence() func() int {
+    i := 0
+    return func() int {
+        i ++
+        return i
+    }
+}
+
+type Circle struct {
+    x, y, radius float64
+}
+
+func(circle Circle) area() float64 {
+    return math.Pi * circle.radius * circle.radius
 }
