4b3abe07a33e8b25137f062d79e518c3bd7b2332
zhanglianxin <zhanglianxin1993@qq.com>
2018-06-28 16:21:31 +0800

    json

---
+++ the_go_programming_language/jsondemo.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+    "encoding/json"
+    "os"
+    "log"
+)
+
+func main() {
+    dec := json.NewDecoder(os.Stdin)
+    enc := json.NewEncoder(os.Stdout)
+
+    for {
+        var v map[string]interface{}
+        if err := dec.Decode(&v); err != nil {
+            log.Println(err)
+            return
+        }
+        for k := range v {
+            if k != "Title" {
+                delete(v, k)
+            }
+        }
+        if err := enc.Encode(&v); err != nil {
+            log.Println(err)
+        }
+    }
+}
