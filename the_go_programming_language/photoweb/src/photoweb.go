023dba42f33705a38ea084ac142f540c7bae09f1
zhanglianxin <zhanglianxin1993@qq.com>
2018-06-29 15:13:06 +0800

    Refactor

--- the_go_programming_language/photoweb/src/photoweb.go
+++ the_go_programming_language/photoweb/src/photoweb.go
@@ -7,7 +7,33 @@ import (
     "os"
     "io/ioutil"
     "html/template"
+    "path"
 )
+
+const (
+    UPLOAD_DIR = "../uploads"
+    TEMPLATE_DIR = "../views"
+)
+
+var templates = make(map[string]*template.Template)
+
+// 对模板进行缓存，即一次性预加载模板。
+// 在初始化时，将所有模板一次性加载到程序中。
+// init() 会在 main() 函数之前执行。
+func init() {
+    fileInfoArr, err := ioutil.ReadDir(TEMPLATE_DIR)
+    check(err)
+    var templateName, templatePath string
+    for _, fileInfo := range fileInfoArr {
+        templateName = fileInfo.Name()
+        if ext := path.Ext(templateName); ext != ".html" {
+            continue
+        }
+        templatePath = TEMPLATE_DIR + "/" + templateName
+        log.Println("Loading template:", templatePath)
+        templates[templateName] = template.Must(template.ParseFiles(templatePath))
+    }
+}
 
 func main() {
     http.HandleFunc("/", listHandler)
@@ -19,38 +45,22 @@ func main() {
     }
 }
 
-const (
-    UPLOAD_DIR = "../uploads"
-    TEMPLATE_DIR = "../views"
-)
-
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
     if r.Method == "GET" {
-        if err := renderHtml(w, TEMPLATE_DIR + "/upload", nil); err != nil {
-            http.Error(w, err.Error(), http.StatusInternalServerError)
-            return
-        }
+        renderHtml(w, "upload", nil)
     }
     if r.Method == "POST" {
         f, h, err := r.FormFile("image")
-        if err != nil {
-            http.Error(w, err.Error(), http.StatusInternalServerError)
-            return
-        }
+        check(err)
         filename := h.Filename
         defer f.Close()
 
         t, err := os.Create(UPLOAD_DIR + "/" + filename)
-        if err != nil {
-            http.Error(w, err.Error(), http.StatusInternalServerError)
-            return
-        }
+        check(err)
         defer t.Close()
 
-        if _, err := io.Copy(t, f); err != nil {
-            http.Error(w, err.Error(), http.StatusInternalServerError)
-            return
-        }
+        _, err = io.Copy(t, f)
+        check(err)
 
         http.Redirect(w, r, "view?id=" + filename, http.StatusFound)
     }
@@ -80,10 +90,7 @@ func isExists(path string) bool {
 
 func listHandler(w http.ResponseWriter, r *http.Request) {
     fileInfoArr, err := ioutil.ReadDir(UPLOAD_DIR)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+    check(err)
 
     locals := make(map[string]interface{})
     images := []string{}
@@ -91,17 +98,16 @@ func listHandler(w http.ResponseWriter, r *http.Request) {
         images = append(images, fileInfo.Name())
     }
     locals["images"] = images
-    if err = renderHtml(w, TEMPLATE_DIR + "/list", locals); err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+    renderHtml(w, "list", locals)
 }
 
-func renderHtml(w http.ResponseWriter, tmpl string, locals map[string]interface{}) (err error) {
-    t, err := template.ParseFiles(tmpl + ".html")
+func renderHtml(w http.ResponseWriter, tmpl string, locals map[string]interface{}) {
+    err := templates[tmpl + ".html"].Execute(w, locals)
+    check(err)
+}
+
+func check(err error) {
     if err != nil {
-        return
+        panic(err)
     }
-    err = t.Execute(w, locals)
-    return
 }
